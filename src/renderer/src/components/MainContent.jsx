a5f921eb1e95375df38efef2c6230ad129cf38d4
Ethan <1028716801@qq.com>
2022-11-22 17:34:51 +0800

    main-tabs静态布局

--- src/renderer/src/components/MainContent.jsx
+++ src/renderer/src/components/MainContent.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { fileList } from '../utils/constant'
+import { Tabs, Badge, createStyles, ScrollArea, Center } from '@mantine/core';
+import { IconSquareX } from '@tabler/icons'
+const useStyles = createStyles((theme, _params, getRef) => ({
+    tab: {
+        '&:hover': {
+            [`& .${getRef('close')}`]: {
+                visibility: 'visible'
+            }
+        },
+    },
+    close: {
+        ref: getRef('close'),
+        visibility: 'hidden',
+    }
+}))
+
+
 
 function MainContent() {
+    const { classes } = useStyles();
+    const [unSaved, setUnsaved] = useState(false)
+
+    const setActiveTab = (id) => {
+        console.log(id);
+    }
     return (
-        <div style={{ height: '100%', width: '100%', backgroundColor: 'pink' }}>
+        <div style={{ height: '100%', width: '100%' }}>
             {/* Tabbar */}
+            <Tabs
+                activateTabWithKeyboard={false}
+                defaultValue={fileList[0].id} onTabChange={setActiveTab} >
+                <Tabs.List >
+                    {fileList.map(item => (
+                        <Tabs.Tab
+                            key={item.id}
+                            className={classes.tab}
+                            value={item.id}
+                            icon={unSaved && (
+                                <Badge
+                                    sx={{ width: 14, height: 14, pointerEvents: 'none' }}
+                                    color='red.3'
+                                    variant="filled"
+                                    size="xs"
+                                    p={0}
+                                ></Badge>
+                            )}
+                            rightSection={
+                                <Center className={classes.close}
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        console.log('close', item.id)
+                                    }}>
+                                    <IconSquareX size={19} />
+                                </Center>
+                            }
+                        >{item.title}</Tabs.Tab>
+                    ))}
+                </Tabs.List>
+                {
+                    fileList.map((item) => (
+                        <Tabs.Panel key={`${item.id}-panel`} value={item.id} pt="xs">
+                            {item.content}
+                        </Tabs.Panel>
+                    ))
+                }
+            </Tabs>
             {/* RichTextEditor */}
         </div>
     )
---
+++ src/renderer/src/utils/constant.js
@@ -0,0 +1,32 @@
+export const fileList = [
+    {
+        id: '1',
+        title: 'first article',
+        content: 'first article',
+        createdAt:1234134
+    },
+     {
+        id: '2',
+        title: 'second article',
+        content: 'second article',
+        createdAt:1234134
+    },
+    {
+        id: '3',
+        title: 'third article',
+        content: 'third article',
+        createdAt:1234134
+    },
+    {
+        id: '4',
+        title: 'fourth article',
+        content: 'fourth article',
+        createdAt:1234134
+    },
+    {
+        id: '5',
+        title: 'fifth article',
+        content: 'fifth article',
+        createdAt:1234134
+    }
+]
